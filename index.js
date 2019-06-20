d6d1993532e0da1366ffa368887ba1c16f50d5a4
C-H-D <416923790@qq.com>
2019-06-21 05:19:32 +0800

    test

--- index.js
+++ index.js
@@ -28,6 +28,8 @@
             event.preventDefault();
         }
         lastTouchEnd = now;
+		if(currentCity != null){document.getElementById('info').src = 'info_'+cities[currentCity]+'-min.jpg';}
+		else{document.getElementById('info').src = 'info-min.jpg';}
     }, false);
 	document.body.addEventListener('touchmove', function (e) { e.preventDefault(); //阻止默认的处理方式(阻止下拉滑动的效果)
 	}, {passive: false}); //passive 参数不能省略，用来兼容ios和android 
