9427616465996bc9388f3e42fcc80e7796376bec
C-H-D <416923790@qq.com>
2019-06-21 03:33:52 +0800

    test

--- index.js
+++ index.js
@@ -14,6 +14,24 @@
             is_phone = false;
         }
 
+	var lastTouchEnd = 0;
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
+
+    // 阻止双指放大
+    document.addEventListener('gesturestart', function(event) {
+        event.preventDefault();
+    });
 	
 	window.addEventListener("orientationchange", reload, false);
 	function reload(){
