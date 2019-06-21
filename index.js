a70415f54ad8878ed4920cc2aabe251f9259b0f1
C-H-D <416923790@qq.com>
2019-06-22 03:41:56 +0800

    ver 1.0

--- index.js
+++ index.js
@@ -7,7 +7,12 @@
 	var currentCity = null;
 	var cities = ['shanghai','beijing','nanjing','weihai','hangzhou','xian','haerbin','nanchang','xiamen','shouer','dongjing','xinjiapo','lizi','manchesite','gelasige','duke','kangnaier','laisi','jiazhouligong','qiaozhicheng']
 	var pos = [[121,31],[116,40],[118.5,31.5],[122,37.4],[120,30],[108,34.1],[126.6,45.5],[115.7,28.5],[118,24.4],[127,37.5],[139.75,35.68],[104,1.3],[-1.5,53.8],[-2.2,53.5],[-4.25,56],[-78.9,36],[-76.48,42.45],[-95.4,29.71],[-118.12,34.136],[-77.59,38.935]]
-	
+	var imgs = [];
+	for(i=0;i<20;i++){
+		imgs[i] = new Image();
+		imgs[i].src = 'info_'+cities[i]+'-min.jpg';
+	}
+	console.log(imgs);
 	if (/Android|webOS|iPhone|iPod|MicroMessenger|BlackBerry/i.test(navigator.userAgent)) {
             is_phone = true;
         } else {
@@ -59,7 +64,7 @@
 		}
 		if(min < 1.5){pause = true;}
 		else{pause = false; currentCity = null;}
-		if(currentCity != null){document.getElementById('info').src = 'info_'+cities[currentCity]+'-min.jpg';}
+		if(currentCity != null){document.getElementById('info').src = imgs[currentCity].src;}
 		else{document.getElementById('info').src = 'info-min.jpg';}
 	}
 	
